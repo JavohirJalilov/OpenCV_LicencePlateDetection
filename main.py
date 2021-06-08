5ceee970839a1e62a9524058278e1c6fae335014
Jalilov <jalilovjavohir777@gmail.com>
2021-06-08 16:01:52 +0500

    path_list finally

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import pathlib
+
+import func
+from func import show, get_img
 
 # pip install -r requirements.txt git
+
+path_images = pathlib.Path('CAR_IMG_600X600').iterdir()
+
+path_list = func.path_to_list(path_images)
+print(path_list)
