97688cf1024816c64e10109a239107b89b3f275a
Jalilov <jalilovjavohir777@gmail.com>
2021-06-08 15:53:31 +0500

    funtion add dock string

--- func.py
+++ func.py
@@ -7,11 +7,18 @@ plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = False
 plt.rcParams['ytick.left'] = plt.rcParams['ytick.labelleft'] = False
 
 def show(img,s=8):
+    '''
+    data visualization
+    '''
     plt.figure(figsize=(s,s))
     plt.imshow(img,cmap='gray')
     plt.show()
 
-def get_img(name):
+def get_img(filename):
+    '''
+    parameter filename.
+    return BGR to RGB image
+    '''
     img = cv2.imread(name)
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     return img
