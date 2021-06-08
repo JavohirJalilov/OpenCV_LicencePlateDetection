cabf0f7e31ae5a8b8865153116d38f1bc582ab4b
Jalilov <jalilovjavohir777@gmail.com>
2021-06-08 16:22:06 +0500

    license plate detection

--- func.py
+++ func.py
@@ -35,9 +35,25 @@ def kernal(x,y):
     k = np.ones((x,y),dtype=np.uint8)
     return k
 
+def max_area_idx(contours):
+    area_list = []
+    for i in range(len(contours)):
+        area_list.append(cv2.contourArea(contours[i]))
+    area_list = np.array(area_list)
+    mx_idx = area_list.argmax()
+    return mx_idx
+
 def license_detection(path_list:list)->None:
     img_path = path_list[0]
     img = get_img(img_path)
     mask = cv2.Canny(img,350,500)
     mask = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernal(9,9))
-    show(mask)
+    mask = cv2.dilate(mask,kernal(5,5))
+    mask = cv2.erode(mask,kernal(6,6))
+
+    contours,_ =cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    mx_idx = max_area_idx(contours)
+    x,y,w,h = cv2.boundingRect(contours[mx_idx])
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
+    cv2.imwrite('detect_license.png',cv2.cvtColor(img,cv2.COLOR_RGB2BGR))
+    show(img)
